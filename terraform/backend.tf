8d940ec8987c8060038bf8b342c68d312d9df14d
Ahmad Dev <se231310@fhstp.ac.at>
2025-06-28 01:26:36 +0200

    fix some problems

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,3 @@
 terraform {
-  backend "azurerm" {
-    container_name       = "tfstate"
-    key                  = "terraform-infra.tfstate"
-    resource_group_name  = "fh-smdo"
-    storage_account_name = "terraform01manual"
-    subscription_id      = "7c21e53b-0e8a-4cd9-8d45-c5905f358e7e"
-    use_azuread_auth     = true
-  }
+  backend "azurerm" {}
 }
