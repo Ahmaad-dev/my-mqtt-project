654754d3bef6f8364e7d3b47e84b2423ce2ec3d9
Ahmad Dev <se231310@fhstp.ac.at>
2025-06-28 01:19:56 +0200

    update backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "fh-smdo"
-    storage_account_name = "terraform01manual"
     container_name       = "tfstate"
     key                  = "terraform-infra.tfstate"
+    resource_group_name  = "fh-smdo"
+    storage_account_name = "terraform01manual"
+    subscription_id      = "7c21e53b-0e8a-4cd9-8d45-c5905f358e7e"
+    use_azuread_auth     = true
   }
 }
