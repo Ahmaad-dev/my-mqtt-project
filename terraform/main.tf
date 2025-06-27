28e447437677911cb90280a3ac89d8789c41c4c4
Ahmaad-dev <se231310@fhstp.ac.at>
2025-06-28 00:56:53 +0200

    fix location for container app

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,6 @@ resource "azurerm_container_app" "app" {
   name                         = var.container_app_name
   container_app_environment_id = azurerm_container_app_environment.env.id
   resource_group_name         = azurerm_resource_group.main.name
-  location                    = var.location
   revision_mode               = "Single"
 
   template {
